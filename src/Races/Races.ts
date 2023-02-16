edba65fe151ba6a38001ce55a92dbf34e12e7e4c
Isabela Costa <isa-hcosta@hotmail.com>
2023-02-16 17:03:54 -0300

    cria classe Race

---
+++ src/Races/Races.ts
@@ -0,0 +1,20 @@
+export default abstract class Race {
+  constructor(
+    private _name: string,
+    private _dexterity: number,
+  ) {}
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
--- src/index.js
+++
@@ -1,1 +0,0 @@
-//iniciano projeto
